d0c4ba808e23d14f3c658ae346f4b1ab45151793
Florencia Ayelen Gerez <fgerez@frba.utn.edu.ar>
2026-05-02 16:00:30 -0300

    Atiende review: Se agregar comentarios a las demas funciones

--- operaciones.c
+++ operaciones.c
@@ -1,26 +1,25 @@
 #include "operaciones.h"
 
 /* ── sumar — ya implementada, referencia de estilo ───────────────────────── */
-
+/* Funcion sumar: calcula y retorna la suma entre los dos enteros recibidos*/
 int sumar(int a, int b) {
     return a + b;
 }
 
 /* ── restar — ya implementada ────────────────────────────────────────────── */
-
+/* Funcion restar: calcula y retorna la resta entre los dos enteros recibidos*/
 int restar(int a, int b) {
     return a - b;
 }
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
 /* Funcion multiplicar: calcula y retorna el producto entre los dos enteros recibidos*/
-
 int multiplicar(int a, int b) {
     return a * b;
 }
 
 /* ── esPar ────────────────────────────────────────────────────────────────── */
-
+/* Funcion esPar: calcula y retorna si el entero recibido es par */
 int esPar(int n) {
     return (n % 2) == 0;
 }
